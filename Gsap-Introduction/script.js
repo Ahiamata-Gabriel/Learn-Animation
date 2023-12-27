711f040858f45aeebefe0040c3334cf792c151e1
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-12-27 02:11:46 +0000

    Animation

--- Gsap-Introduction/script.js
+++ Gsap-Introduction/script.js
@@ -1,1 +1,6 @@
-gsap.to('.logo', { duration: 2, x: 300 });
+gsap.to('.logo', { duration: 2, x: 300, ease: 'bounce.inOut' });
+gsap.to('#demo', {
+  duration: 5,
+  backgroundColor: '#560563',
+  ease: 'slow(0.7,0.7,false)',
+});
