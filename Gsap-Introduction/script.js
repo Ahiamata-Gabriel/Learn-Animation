629afd13f2a1e84789caaa13a6cec4e05ec19524
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-01-03 02:06:15 +0000

    Intro to Gsap

--- Gsap-Introduction/script.js
+++ Gsap-Introduction/script.js
@@ -1,13 +1,4 @@
-gsap.to('.logo', { duration: 2, x: 300, ease: 'bounce.inOut' });
-gsap.to('#demo', {
-  duration: 5,
-  backgroundColor: '#560563',
-  ease: 'slow(0.7,0.7,false)',
-});
-
-gsap.from('.hero__image', {
-  duration: 1.5,
-  scale: 0.3,
-  ease: 'in',
-  x: 50,
+gsap.to('.box', {
+  duration: 2,
+  backgroundColor: '#8d3dae',
 });
