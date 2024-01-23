a5137212072535ba6999f0eb6a6dd2b63f749da2
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-01-23 00:07:08 +0000

    Gsap

--- Gsap-Introduction/script.js
+++ Gsap-Introduction/script.js
@@ -1,4 +1,14 @@
-gsap.to('.box', {
-  duration: 2,
-  backgroundColor: '#8d3dae',
-});
+// gsap.to('.box', {
+//   duration: 2,
+//   backgroundColor: '#8d3dae',
+// });
+
+// gsap.to('.box', {
+//   rotation: 360,
+//   x: '100vw',
+//   xPercent: 100,
+//   // special properties
+//   duration: 2, // how long the animation lasts
+//   repeat: 2, // the number of repeats - this will play 3 times
+//   yoyo: true, // this will alternate back and forth on each repeat. Like a yoyo
+// });
