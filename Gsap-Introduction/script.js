3c2cda81fbfb3546041d997de4a67a558e27e393
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-01-24 22:03:47 +0000

    Run code on click

--- Gsap-Introduction/script.js
+++ Gsap-Introduction/script.js
@@ -12,3 +12,18 @@
 //   repeat: 2, // the number of repeats - this will play 3 times
 //   yoyo: true, // this will alternate back and forth on each repeat. Like a yoyo
 // });
+
+// gsap.from('.box', { x: 400, y: 200, scale: 3, duration: 3 });
+
+document.querySelectorAll('.box').forEach(function (box) {
+  box.addEventListener('click', function () {
+    gsap.to('.box', {
+      duration: 0.5,
+      opacity: 0,
+      y: -100,
+      stagger: 0.1,
+      ease: 'back.in',
+    });
+  });
+});
+d;
