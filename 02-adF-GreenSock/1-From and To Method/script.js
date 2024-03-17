168219fc73173b0863398ac21c3ef5aa6aca4b63
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-17 11:38:28 +0000

    From and to Method

--- 02-adF-GreenSock/1-From and To Method/script.js
+++ 02-adF-GreenSock/1-From and To Method/script.js
@@ -1,13 +1,14 @@
-gsap.to('.progress', {
-  width: '100%',
+gsap.to(".progress", {
+  width: "100%",
   duration: 2,
-  ease: 'power4.out',
+  ease: "power4.out",
 });
 
-gsap.from('h1', { y: -200, ease: 'elastic.out(1,0.3)', duration: 2 });
+gsap.from("h1", { y: -200, ease: "elastic.out(1,0.3)", duration: 2 });
 
-gsap.to('.progress', {
-  height: '100%',
+gsap.to(".progress", {
+  height: "100%",
   top: 0,
   delay: 2,
+  backgroundColor: "white",
 });
